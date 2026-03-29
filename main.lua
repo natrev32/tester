79dd467b32d012e1f312290a2f61841d75431a8b
rev <smithdfno@gmail.com>
2026-03-29 18:00:17 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,28 @@
+repeat task.wait() until game:IsLoaded()
+
+print("Script Loaded")
+
+local Library = loadstring(game:HttpGet("LINK_UI_LIBRARY"))()
+
+local v31 = Library:CreateWindow({
+    Title = "Brainrot Script"
+})
+
 local InstantInteractionToggle = v31:Toggle({
     Title = "Instant Interaction",
-    Desc = "Mempercepat semua interaksi (brainrot / prompt)",
+    Desc = "Mempercepat semua interaksi",
     
     Callback = function(state)
         if state then
-            -- Aktif
-            print("Instant Interaction ON")
+            print("ON")
 
-            -- Contoh logic (edit sesuai game)
             for _, v in pairs(game:GetDescendants()) do
                 if v:IsA("ProximityPrompt") then
                     v.HoldDuration = 0
                 end
             end
-
         else
-            -- Nonaktif
-            print("Instant Interaction OFF")
+            print("OFF")
         end
     end
 })
